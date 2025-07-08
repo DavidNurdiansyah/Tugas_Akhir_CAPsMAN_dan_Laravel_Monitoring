8662db7f409b3c30f902e5c1c52b0d3e10d0e55f
DavidNurdiansyah <dafidnurdiansyah74@gmail.com>
2025-07-08 21:17:46 +0700

    Menghapus baris kode pengujian

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    // protected $fillable = ['text', 'bandwidth'];
+
     protected $fillable = ['bandwidth', 'text'];
     public $timestamps = true; // Ini aktifkan otomatis created_at & updated_at
 }
